e9d43cc557140724ca85a2417f30b94104a15741
Nandagopal Menon <nandagopalnmenon@gmail.com>
2024-10-24 00:15:35 +0530

    bouta kill myself rn

--- bot.py
+++ bot.py
@@ -1,56 +1,105 @@
-from game import ChessBoard, Pawn, Piece
+from game import ChessBoard, Pawn, Piece, BLACK, WHITE
+
 
 class Bot:
-    def evaluate(self,board):        
+    def evaluate(self, board):
         score = 0
         for row in range(8):
-            for col in range (8):
-                score+=self.get_piece_val(board,(row,col))
-        return score         
+            for col in range(8):
+                score += self.get_piece_val(board, (row, col))
+        return score
 
-    def minimax(self, board: ChessBoard, depth=0, to_min=True):
+    def minimax(self, board: ChessBoard, depth=0, white_turn=False):
         if depth == 0 or board.is_checkmate():
-            return self.evaluate(board)
+            return self.evaluate(board), (-1, -1)
 
-        ret = float("inf") if to_min else float("-inf")
         valid_moves = []
         for row in range(8):
             for col in range(8):
-                moves = board.get_valid_moves((row, col))
-                if moves: 
-                    valid_moves.extend(list(map(lambda x: ((row, col), x), moves)))
+                if (piece := board.get_piece((row, col))) != " " and (
+                    (piece.color == WHITE and white_turn)
+                    or (piece.color == BLACK and not white_turn)
+                ):
+                    moves = board.get_valid_moves((row, col))
+                    if moves:
+                        valid_moves.extend(list(map(lambda x: ((row, col), x), moves)))
 
-        return valid_moves
+        ret = float("inf") if white_turn else float("-inf")
+        ret_move = ((-1, -1), (-1, -1))
+        for move in valid_moves:
+            clone = board.clone()
+            clone.move_piece(*move)
+            val, move = self.minimax(clone, depth - 1, not white_turn)
+
+            if (val < ret and not white_turn) or (val > ret and white_turn):
+                ret = val
+                ret_move = move
+
+        return ret, ret_move
 
     def get_piece_val(self, board, pos):
-        '''
+        """
         This funciton should return the value of a piece in a given position in the gien board
-        '''
+        """
 
         piece = board.get_piece(pos)
         row, col = pos
         if isinstance(piece, Piece):  # Check if the piece is a pawn
-            if piece.color=='black':
-                return -(piece.bonus[::-1][row][col]+piece.value)
-            elif piece.color=='white':
-                return piece.bonus[row][col]+piece.value
+            if piece.color == "black":
+                return -(piece.bonus[::-1][row][col] + piece.value)
+            elif piece.color == "white":
+                return piece.bonus[row][col] + piece.value
         return 0  # Default if no bonus is applicable
 
 
-if __name__ == "__main__":
+def play_vs_bot():
     board = ChessBoard()
     bot = Bot()
-    board.print_board()
-    moves = bot.minimax(board, 1)
-    print(len(moves))
 
-    print(board.get_valid_moves((6, 0)))
+    while True:
+        board.print_board()
 
-    # for start, stop in moves:
-    #     clone = board.clone()
-    #     clone.move_piece(start, stop)
-    #     print(start, stop)
-    #     clone.print_board()
-    #     print("-----------------------")
-    
+        # Player move (simple manual input for demonstration)
+        start = tuple(
+            input(
+                f"{board.turn.capitalize()}'s turn. Enter start position (FileRank): "
+            ).upper()
+        )
+        start = ChessBoard.file_rank_to_coords(start[0], start[1])
+        end = tuple(input("Enter the end position (FileRank): ").upper())
+        end = ChessBoard.file_rank_to_coords(end[0], end[1])
 
+        if board.is_valid_move(start, end):
+            board.move_piece(start, end)
+
+            # Switch turns
+            board.turn = BLACK
+
+            # Check for checkmate
+
+            if board.is_in_check(board.turn):
+                if board.check_checkmate():
+                    print(f"{'Black' if board.turn == WHITE else 'White'} wins!")
+                    break
+                else:
+                    print("check!!")
+
+            val, move = bot.minimax(board, 2, False)
+
+            board.move_piece(*move)
+
+            board.turn = WHITE
+
+            if board.is_in_check(board.turn):
+                if board.check_checkmate():
+                    print(f"{'Black' if board.turn == WHITE else 'White'} wins!")
+                    break
+                else:
+                    print("check!!")
+
+        else:
+            print("Invalid move. Try again.")
+
+
+if __name__ == "__main__":
+    play_vs_bot()
